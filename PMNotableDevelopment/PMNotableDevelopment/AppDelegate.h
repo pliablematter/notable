3e56b5b0000fb724af56a728d1e3af7e647f97f9
Igor Milakovic <igor@cobeisfresh.com>
2014-10-25 09:34:50 +0200

    Initial project commit

---
+++ PMNotableDevelopment/PMNotableDevelopment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PMNotableDevelopment
+//
+//  Created by Igor Milakovic on 25/10/14.
+//  Copyright (c) 2014 Pliable Matter LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
