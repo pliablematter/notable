4b6fe88dd27d4c288e1b52dae8608cfc327985bd
Igor Milakovic <igor@cobeisfresh.com>
2014-10-25 09:25:05 +0200

    PMNotable library preparation

---
+++ PMNotable/PMNotable.h
@@ -0,0 +1,13 @@
+//
+//  PMNotable.h
+//  PMNotable
+//
+//  Created by Igor Milakovic on 25/10/14.
+//  Copyright (c) 2014 Pliable Matter LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMNotable : NSObject
+
+@end
---
+++ PMNotable/PMNotable.m
@@ -0,0 +1,13 @@
+//
+//  PMNotable.m
+//  PMNotable
+//
+//  Created by Igor Milakovic on 25/10/14.
+//  Copyright (c) 2014 Pliable Matter LLC. All rights reserved.
+//
+
+#import "PMNotable.h"
+
+@implementation PMNotable
+
+@end
